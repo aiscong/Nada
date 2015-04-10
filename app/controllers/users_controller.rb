8955d74ba270d35f527e7721370e380bf69f7148
aiscong <aiscong@gmail.com>
2015-04-10 05:51:10 +0000

    json support

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,11 +26,14 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-    if @user.save
-      flash[:success] = "Success: create user"
-      redirect_to @user
-    else
-      render 'new'
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully updated' }
+        format.json { render :show, status: :ok, location: @user}
+      else
+        format.html { render :new}
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
