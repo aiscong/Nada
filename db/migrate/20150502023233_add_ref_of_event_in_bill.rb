8bb81b436e43b73c1d7d06fb5a5ac69708fae4a4
aiscong <aiscong@gmail.com>
2015-05-03 03:56:21 +0000

    adjust migrations

--- db/migrate/20150502023233_add_ref_of_event_in_bill.rb
+++ db/migrate/20150502023233_add_ref_of_event_in_bill.rb
@@ -1,6 +1,6 @@
 class AddRefOfEventInBill < ActiveRecord::Migration
   def change
-    add_reference :bills, :events, index: true
-    add_foreign_key :bills, :events
+    #add_reference :bills, :events, index: true
+    #add_foreign_key :bills, :events
   end
 end
--- db/migrate/20150503013752_remove_wrong_index_of_bills.rb
+++ db/migrate/20150503013752_remove_wrong_index_of_bills.rb
@@ -1,6 +1,6 @@
 class RemoveWrongIndexOfBills < ActiveRecord::Migration
   def change
-    remove_reference :bills, :events, index: true
-    remove_foreign_key :bills, :events
+    #remove_reference :bills, :events, index: true
+    #remove_foreign_key :bills, :events
   end
 end
